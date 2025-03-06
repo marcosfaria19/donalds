32df368cd3840e0a802fe80a08631a3ebde3bc77
Marcos Faria <marcos.faria19@hotmail.com>
2025-03-06 11:06:22 -0300

    chore: Add Prettier TailwindCSS Plugin and ESLint Sort Plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const ProductPage = () => {
-  return <h1>Product Page</h1>;
+  <>
+    <h1 className="bg-red-500 p-5">Product Page</h1>;
+    <Button className="bg-red-500 p-5">Teste</Button>
+  </>;
 };
 
 export default ProductPage;
